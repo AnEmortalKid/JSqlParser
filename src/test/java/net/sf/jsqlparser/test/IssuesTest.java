2a51aa1301516d0b3d4772c016fb477f3992f0e0
AnEmortalKid <janmonterrubio@gmail.com>
2020-06-21 16:25:08 -0500

    more wip

--- src/test/java/net/sf/jsqlparser/test/IssuesTest.java
+++ src/test/java/net/sf/jsqlparser/test/IssuesTest.java
@@ -11,6 +11,7 @@ package net.sf.jsqlparser.test;
 
 import net.sf.jsqlparser.parser.CCJSqlParserUtil;
 import net.sf.jsqlparser.statement.Statement;
+import net.sf.jsqlparser.statement.create.index.CreateIndex;
 import net.sf.jsqlparser.util.TablesNamesFinder;
 import org.junit.Test;
 
@@ -30,16 +31,7 @@ public class IssuesTest {
     @Test
     public void nextIssue() throws Exception {
         Statement stmt = CCJSqlParserUtil.parse(
-                "SELECT C_DocType.C_DocType_ID,NULL,COALESCE(C_DocType_Trl.Name,C_DocType.Name) " +
-                        "AS Name,C_DocType.IsActive FROM C_DocType LEFT JOIN C_DocType_TRL " +
-                        "ON (C_DocType.C_DocType_ID=C_DocType_Trl.C_DocType_ID " +
-                        "AND C_DocType_Trl.AD_Language='es_AR') " +
-                        "WHERE C_DocType.AD_Client_ID=1010016 AND C_DocType.AD_Client_ID IN (0,1010016) " +
-                        "AND C_DocType.c_doctype_id in " +
-                        "( select c_doctype2.c_doctype_id from c_doctype as c_doctype2 " +
-                        "where substring( c_doctype2.printname from length(c_doctype2.printname) ) " +
-                        "= ( select letra from c_letra_comprobante as clc where clc.c_letra_comprobante_id = 1010039) ) " +
-                        "AND ( (1010094!=0 AND C_DocType.ad_org_id = 1010094) OR 1010094=0 ) ORDER BY 3 LIMIT 2000");
+                "SELECT {fn CURRENT_TIMESTAMP(0)} AS COL");
         System.out.println(stmt);
     }
 }
